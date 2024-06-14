9e22589c120af06915978604bbbf1735851388e8
SuperRobs <superrobsyt@gmail.com>
2024-06-14 15:13:11 +0200

    Setup application structure

---
+++ SuperDungeons/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace SuperDungeons;
+
+public partial class App
+{
+    
+}
---
+++ SuperDungeons/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperDungeons;
+
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
