b7f16286fcb00ae0ab41c2080e58acac28da214f
SuperRobs <superrobsyt@gmail.com>
2024-08-16 19:34:03 +0200

    added Subclass and ClassFeatures to Class

--- SuperDungeons/Model/Classes/Class.cs
+++ SuperDungeons/Model/Classes/Class.cs
@@ -1,9 +1,10 @@
 ﻿namespace SuperDungeons.Model.Classes;
 
-public class Class(string className)
+internal class Class(string className)
 {
     private ClassDefinition _classDefinition = ClassDefinition.GetClassDefinition(className);
+    public Subclass? subclass;
     public uint ClassLevel;
-    
-    
+    public List<ClassFeature> features;
+
 }
