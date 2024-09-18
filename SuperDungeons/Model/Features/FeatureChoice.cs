6d3517a79b4666f02821a35bb2421a11d7fea4c1
SuperRobs <superrobsyt@gmail.com>
2024-09-18 15:14:38 +0200

    make FeatureChoice a record

--- SuperDungeons/Model/Features/FeatureChoice.cs
+++ SuperDungeons/Model/Features/FeatureChoice.cs
@@ -1,13 +1,9 @@
 namespace SuperDungeons.Model.Features;
 
-internal class FeatureChoice(
+internal record FeatureChoice(
     //the Identifier references the ChoiceFeature which contains the Subfeatures
-    FeatureIdentifier parent,
+    FeatureIdentifier Parent,
     //the amount of possible choices depends on the Feature, but can not be validated here. If too many choices are made
     //only the first ones will be used
     //these Identifiers reference the chosen Subfeatures
-    List<FeatureIdentifier> choices)
-{
-    public FeatureIdentifier Parent { get; } = parent;
-    public List<FeatureIdentifier> Choices { get; } = choices;
-}
+    List<FeatureIdentifier> Choices);
