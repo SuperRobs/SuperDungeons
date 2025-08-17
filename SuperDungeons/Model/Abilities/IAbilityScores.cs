8db19b689be36cc43e4928a46751ec836104d4a1
Robin <robin.gerhard.janssen@gmail.com>
2025-08-17 15:20:47 +0200

    replaced AbilityScoreBonus in Parameters

--- SuperDungeons/Model/Abilities/IAbilityScores.cs
+++ SuperDungeons/Model/Abilities/IAbilityScores.cs
@@ -8,7 +8,7 @@ public interface IAbilityScores
     public int GetAbilityModifier(Ability ability);
      
     public void AddOverride(Ability ability, FeatureIdentifier source, uint value);
-    public void AddBonus(Ability ability, FeatureIdentifier source, AbilityScoreBonus bonus);
+    public void AddBonus(Ability ability, FeatureIdentifier source, int bonus, uint cap = 20);
     
     public void RemoveOverride(Ability ability, FeatureIdentifier source);
     public void RemoveBonus(Ability ability, FeatureIdentifier source);
